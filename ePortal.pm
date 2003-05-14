4f8b13d4d905e69b94d1c4433d562c1d885c3f6c
Sergey Rusakov <rusakov_sa@users.sourceforge.net>
2003-05-14 09:06:50 +0000

    Import of RUSAKOV/Bundle-ePortal-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-ePortal
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         RUSAKOV/Bundle-ePortal-0.01.tar.gz
    gitpan-cpan-author:       RUSAKOV
    gitpan-cpan-maturity:     released

---
+++ ePortal.pm
@@ -0,0 +1,110 @@
+package Bundle::ePortal;
+
+$VERSION = 0.01;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::ePortal - Bundle with all ePortal related modules
+
+
+
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::ePortal'>
+
+
+
+
+
+=head1 CONTENTS
+
+Apache
+
+Apache::Constants
+
+Apache::Cookie
+
+Apache::Request
+
+Apache::Util
+
+Bundle::HTML::Mason
+
+CGI
+
+Convert::Cyrillic
+
+Data::Dumper
+
+Date::Calc
+
+DBD::mysql
+
+DBI
+
+Digest::MD5
+
+Error
+
+File::Basename
+
+Image::Size
+
+List::Util
+
+Mail::Sendmail
+
+MIME::Base64
+
+MD5
+
+mod_perl
+
+Net::LDAP
+
+Params::Validate
+
+Storable
+
+Text::Wrap
+
+Unicode::Map8
+
+Unicode::String
+
+
+
+
+
+
+
+=head1 DESCRIPTION
+
+This bundle contains all modules related to the ePortal project.
+See http://eportal.sourceforge.net/ for details about it.
+
+
+
+
+
+
+
+=head1 AUTHOR
+
+Sergey Rusakov, E<lt>rusakov_sa@users.sourceforge.netE<gt>
+
+
+
+
+
+
+=head1 SEE ALSO
+
+http://eportal.sourceforge.net/
+
+=cut
